bbe051fc42cb4370fc5ac3f393a1577d50af37d6
a01173530 <A01173530@itesm.mx>
2021-05-01 16:03:40 -0500

    Multer

--- UMA/index.js
+++ UMA/index.js
@@ -19,6 +19,8 @@ app.use(session({
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
+const multer = require('multer');
+
 const path = require('path');
 const csrf = require('csurf');
 const csrfProtection = csrf();
